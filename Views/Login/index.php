13fb05b6c5902394bae33d397ceac36894bc81dd
mickey2099beyond <43194494+mickey2099beyond@users.noreply.github.com>
2018-10-16 19:38:14 -0500

    validacionformulariologin
    scripts con validacion para el formulario login

--- Views/Login/index.php
+++ Views/Login/index.php
@@ -6,6 +6,10 @@
   </div>
   <div class="col-md-4">
     <form class="form-signin">
+
+      <script type="text/javascript" src="../js/jquery-1.9.1.js"></script>
+      <script type="text/javascript" src="../js/jquery.validate.js"></script>
+      <script type="text/javascript" src="../js/jquery.validate.min.js"></script>
   <h1 class="h3 mb-3 font-weight-normal text-center">Iniciar Sesion</h1>
   <label for="inputEmail" class="sr-only">Usuario</label>
   <input type="email" id="inputEmail" class="form-control" placeholder="usuario" required="" autofocus="">
@@ -22,5 +26,30 @@
   <div class="col-md-4">
   </div>
 </div>
+  <script>
+var = expr = /^[a-zA-Z0-9\.\-]+@[a-zA-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+
+$(document).ready(function(){
+$("#Entrar").click(function(){
+	var correo = $("#inputemail").val();
+  var nombre =$("#inputPassword").val();
+
+
+
+
+if(correo =="" || !expr.test(correo)){
+alert("ingresa un correo valido porfavor").fadeIn();
+return false;
+}
+
+if(password == ""){
+
+	alert("Por favor, llena este campo.").fadeIn();
+	return false;
+}
+
+});
+});
+</script>
 </div>
 
