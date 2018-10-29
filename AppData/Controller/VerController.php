1f39f722bfa5e5f8035662b953150511689df547
mickey2099beyond <43194494+mickey2099beyond@users.noreply.github.com>
2018-10-29 17:31:56 -0600

    funcion eliminar calificaciones
    funcion que elimina las calificaciones desde la base

--- AppData/Controller/VerController.php
+++ AppData/Controller/VerController.php
@@ -10,8 +10,25 @@
     }
     function index()
     {
+      echo "hola";
+        $datos=$this->ver->getAlumns();
+        return $datos;
 
     }
+    
+    function ver(){
+
+}
+
+function eliminar(){
+
+  $this->calificaciones->set("id",$id);
+  $this->calificaciones->delete();
+
+  ?>
+  <script type="text/javascript"></script>
+  <?php
+}
     function __destruct()
     {
 
