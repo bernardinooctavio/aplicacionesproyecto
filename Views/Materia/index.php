8d4c2cfdf57a5bb6fd42bd04d663920c123b9519
mickey2099beyond <43194494+mickey2099beyond@users.noreply.github.com>
2018-10-31 01:27:21 -0600

    vistamateria
    vistamateria

--- Views/Materia/index.php
+++ Views/Materia/index.php
@@ -1,3 +1,37 @@
-<div class="container" style="margin-top:5em;">
-<p> Materia</p>
-</div>
+<table class="table">
+<h3></h3>
+<h4>Materias</h4>
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Clave</th>
+      <th scope="col">Materias</th>
+      <th scope="col">Unidades</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+
+while($filas=mysqli_fetch_assoc($datos))
+{
+
+  ?>
+
+    <tr>
+      <th scope="row"><?php echo $filas['id_materia'] ?></th>
+      <td><?php echo $filas['desc_materia'] ?></td>
+      <td><?php echo $filas['no_unidades'] ?></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+       <td></td>
+
+    </tr>
+    <?php
+}
+
+?>
+
+</tbody>
+</table>
