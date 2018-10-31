90286b70d29075c97286c6b834dc6ebe7aecd321
mickey2099beyond <43194494+mickey2099beyond@users.noreply.github.com>
2018-10-31 00:02:05 -0600

    vistagrupo
    vistagrupo

--- Views/Grupo/index.php
+++ Views/Grupo/index.php
@@ -1,3 +1,26 @@
-<div class="container" style="margin-top:5em;">
-<p>Grupos</p>
-</div>
+<table class="table">
+<h3>Sistemas</h3>
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Grupos</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+while($filas=mysqli_fetch_assoc($datos))
+{ ?>
+
+    <tr>
+      <th scope="row"><?php echo $filas['id_grupo'] ?></th>
+        <th scope="row"><?php echo $filas['desc_grupo'] ?></th>
+      <td></td>
+      <td></td>
+    </tr>
+<?php
+}
+
+?>
+</tbody>
+</table>
+
