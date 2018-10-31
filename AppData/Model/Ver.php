1dcc8524900e5a6b00b966963b6a96f0ec1de53a
mickey2099beyond <43194494+mickey2099beyond@users.noreply.github.com>
2018-10-30 23:51:34 -0600

    funcionalumnos
    funcionarreglada

--- AppData/Model/Ver.php
+++ AppData/Model/Ver.php
@@ -23,8 +23,20 @@
           $sql="SELECT u.id_usuario,p.nombre_per,p.ap,p.am
           FROM personas p,usuarios u
           WHERE p.id_usuario=u.id_usuario
-          AND u.id_tusuario=1
+          AND u.id_tusuario
           ORDER BY p.ap ASC";
+          echo $sql;
+          $datos=$this->conexion->QueryResultado($sql);
+          return $datos;
+
+        }
+
+        public function getunidad1(){
+          $sql="SELECT c.id_calificacion,c.id_persona,c.calificacion
+          FROM calificaciones c
+          WHERE c.id_calificacion=c.id_persona
+          AND c.calificacion
+          ORDER BY c.id_persona ASC";
           echo $sql;
           $datos=$this->conexion->QueryResultado($sql);
           return $datos;
