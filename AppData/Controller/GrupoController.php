6f1535a918e86d1824716509f8d4379c9d114d23
mickey2099beyond <43194494+mickey2099beyond@users.noreply.github.com>
2018-11-08 04:20:26 -0600

    funciones
    ok

--- AppData/Controller/GrupoController.php
+++ AppData/Controller/GrupoController.php
@@ -8,13 +8,60 @@
     {
       $this->grupo=new Grupo();
     }
-    
+
     function index()
     {
       echo "hola";
       $datos=$this->grupo->getGrupo();
       return $datos;
     }
+
+		function eliminar($id){
+			$this->grupo->set("id",$id);
+			$this->grupo->delete();
+			?>
+			<script type="text/javascript">
+				$(document).ready(function(){
+					swal({
+						title: "Listo",
+						text: "Eliminado",
+						timer: 2000
+					});
+					setTimeout(function(){
+						window.location.href="<?php echo URL ?>Grupo/index"
+					},2100);
+				})
+			</script>
+			<?php
+		}
+    function get($id){
+      $this->grupo->set("id",$id);
+      $datos=$this->grupo->getOne();
+      if(mysqli_num_rows($datos)>0){
+        $datos=mysqli_fetch_assoc($datos);
+      }
+      echo json_encode($datos);
+    }
+    function edit(){
+			$data=$_POST['arreglo'];
+			$this->grupo->set("id",$data[0]['value']);
+			$this->grupo->set("desc_grupo",$data[1]['value']);
+			$this->grupo->updateGrupo();
+      ?>
+      <script type="text/javascript">
+        $(document).ready(function(){
+          swal({
+            title: "Success Listo !!!!",
+            text: "Eliminado correctamente",
+            timer: 2000
+          });
+          setTimeout(function(){
+            window.location.href="<?php echo URL ?>Ver/index"
+          },2100);
+        })
+      </script>
+      <?php
+		}
     function __destruct()
     {
 
@@ -22,3 +69,4 @@
   }
 
  ?>
+
