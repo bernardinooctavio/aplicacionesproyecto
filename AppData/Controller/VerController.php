4c622460f5de7cb9aaf56f79ffa8e2234252dee5
bernardinooctavio <villegas_3101@hotmail.com>
2018-11-24 13:55:38 -0600

    mostraralumnos
    mostrar alumnos y calificaciones

--- AppData/Controller/VerController.php
+++ AppData/Controller/VerController.php
@@ -3,77 +3,42 @@
   use AppData\Model\Ver;
   class VerController
   {
+      private $ver;
+      function __construct(){
+          $this->ver=new Ver();
+      }
+      function index(){
+          $datos=$this->ver->getAlumns();
+          return $datos;
 
-    private $ver;
-    function __construct()
-    {
-      $this->ver=new Ver();
-    }
-    function index()
-    {
+      }
+      function ver(){
 
-      $datos[0]=$this->ver->getAlumns();
-      $datos[1]=$this->ver->getunidad1();
-      return $datos;
+      }
+      function eliminar(){
+          $this->calificaciones->set("id",$id);
+          $this->calificaciones->delete();
+          ?>
+          <script type="text/javascript">
 
-}
-    function eliminar($id){
+          </script>
+          <?php
+      }
+      function edit(){
+          $data=$_POST['arreglo'];
+          $this->calificaciones->set("id",$data[0]['value']);
+          $this->calificaciones->set("nombre",$data[1]['value']);
+          $this->calificaciones->set("ap_p",$data[2]['value']);
+          $this->calificaciones->set("ap_m",$data[3]['value']);
+          $this->calificaciones->set("edad",$data[4]['value']);
+          $this->calificaciones->updatePer();
+          ?>
+          <script type="text/javascript">
 
-			$this->ver->set("id",$id);
-			$this->ver->delete();
+          </script>
+          <?php
+      }
+      function __destruct(){
 
-			?>
-			<script type="text/javascript">
-				$(document).ready(function(){
-					swal({
-						title: "Listo !!!!",
-						text: "Se ha eliminado correctamente",
-						timer: 2000
-					});
-					setTimeout(function(){
-						window.location.href="<?php echo URL ?>Ver/index"
-					},2100);
-				})
-			</script>
-			<?php
-		}
-		function get($id){
-			$this->ver->set("id",$id);
-			$datos=$this->ver->getOne();
-			if(mysqli_num_rows($datos)>0){
-				$datos=mysqli_fetch_assoc($datos);
-			}
-			echo json_encode($datos);
-		}
-		function edit(){
-			$data=$_POST['arreglo'];
-			$this->ver->set("id",$data[0]['value']);
-			$this->ver->set("nombre_per",$data[1]['value']);
-			$this->ver->set("ap",$data[2]['value']);
-			$this->ver->set("am",$data[3]['value']);
-			$this->ver->updatePer();
-      ?>
-      <script type="text/javascript">
-        $(document).ready(function(){
-          swal({
-            title: "Listo !!!!",
-            text: "Eliminado correctamente",
-            timer: 2000
-          });
-          setTimeout(function(){
-            window.location.href="<?php echo URL ?>Ver/index"
-          },2100);
-        })
-      </script>
-      <?php
-		}
-
-
-
-    function __destruct()
-    {
-
-    }
+      }
   }
-
- ?>
--- AppData/Model/Ver.php
+++ AppData/Model/Ver.php
@@ -2,69 +2,64 @@
     namespace AppData\Model;
     class Ver
       {
-
-	     	private $id,$nombre_per,$ap,$am;
-        function __construct()
-        {
-          $this->conexion=new conexion();
+        private $id, $nombre, $ap_p, $ap_m;
+        //private $conexion
+        public function __construct(){
+            $this->conexion= new conexion();
         }
-        public function set($atributo,$valor)
-        {
-          $this->$atributo=$valor;
+        public function set($atributo,$valor){
+            $this->$atributo=$valor;
         }
-        public function get($atributo)
-        {
-          return $this->$atributo;
+        public function get($atributo){
+            return $this->$atributo;
         }
-
-          public function getAlumns(){
-          $sql="SELECT u.id_usuario,p.nombre_per,p.ap,p.am
-          FROM personas p,usuarios u
-          WHERE p.id_usuario=u.id_usuario
-          AND u.id_tusuario
-          ORDER BY p.ap ASC";
-          $datos=$this->conexion->QueryResultado($sql);
-          return $datos;
-
+        public function getAlumns(){
+            //consulta
+            $sql="SELECT u.id_usuario, p.nombre, p.ap_p, p.ap_m
+            FROM persona p, usuario u
+            WHERE p.id_usuario=u.id_usuario
+            AND u.id_tipo_usuario=1
+            ORDER BY p.ap_p ASC";
+            $datos=$this->conexion->QueryResultado($sql);
+            return $datos;
+        }
+        public function delete() {
+            //echo $this->id;
+            $sql="DELETE FROM usuario
+      WHERE id_usuario='{$this->id}'";
+            $this->conexion->QuerySimple($sql);
+            $sql="DELETE FROM persona
+      WHERE id_usuario='{$this->id}'";
+            $this->conexion->QuerySimple($sql);
+            //echo $sql;
         }
 
         public function getOne(){
-    $sql="SELECT u.id_usuario, p.nombre, p.ap, p.am
-          FROM personas p, usuarios u
-          WHERE p.id_usuario=u.id_usuario
-          AND u.id_tusuario=1
-          AND p.id_usuario='{$this->id_usuario}'
-          ORDER BY p.ap ASC";
-    $datos=$this->conexion->QueryResultado($sql);
-    return $datos;
-  }
-
-        public function getunidad1(){
-
-            $sql=" SELECT calificacion FROM calificaciones ";
-
-            //echo $sql;
+            $sql="SELECT u.id_usuario, p.nombre, p.ap_p, p.ap_m
+            FROM persona p, usuario u
+            WHERE p.id_usuario=u.id_usuario
+            AND u.id_tipo_usuario=1
+            AND p.id_usuario='{$this->id}'
+            ORDER BY p.ap_p ASC";
             $datos=$this->conexion->QueryResultado($sql);
-
-           // $this->conexion->QuerySimple($sql);
             return $datos;
 
-           // $this->conexion->QuerySimple($sql);
-
-
+            echo $sql;
         }
-         function delete($id){
-			$sql="DELETE FROM usuarios
-			WHERE id_usuario='{$this->id}'";
-			$this->conexion->QuerySimple($sql);
-			$sql="DELETE FROM personas
-			WHERE id_usuario='{$this->id}'";
-			$this->conexion->QuerySimple($sql);
-		}
-        public function updatePer(){
-    $sql="UPDATE personas SET nombre_per='{$this->nombre_per}', ap='{$this->ap}', am='{$this->am}' WHERE id_usuario='$this->id_usuario'";
-    $this->conexion->QuerySimple($sql);
-  }
-}
 
- ?>
+        public function updatePer(){
+            $sql="UPDATE persona SET nombre='{$this->nombre}', ap_p='{$this->ap_p}', ap_m='{$this->ap_m}', edad='{$this->edad}' WHERE id_usuario='$this->id'";
+            $this->conexion->QuerySimple($sql);
+        }
+        public function getMat(){
+            $sql="SELECT * FROM materias";
+            $datos=$this->conexion->QueryResultado($sql);
+            return $datos;
+        }
+
+
+
+
+
+    }
+    ?>
